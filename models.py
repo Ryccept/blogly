0aa4ccee45757f9f7a625efeba4b1c2515439aeb
Ryan Torre <rtorre1799@gmail.com>
2022-12-04 00:24:23 -0500

    fixed edit and added cascade

--- models.py
+++ models.py
@@ -27,7 +27,7 @@ class User(db.Model):
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
 
-    posts = db.relationship("Post", backref="user")
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
 
 class Post(db.Model):
     '''User Database.'''
