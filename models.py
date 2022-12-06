4e5e54fecc82a33eb6e7249ade7803f871fde31f
Ryan Torre <rtorre1799@gmail.com>
2022-12-05 23:10:53 -0500

    added new models: tag and posttag

--- models.py
+++ models.py
@@ -45,3 +45,39 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    tags = db.relationship('Tag',
+                               secondary='posttags',
+                               backref='posts')
+
+
+
+class Tag(db.Model):
+    '''User Database.'''
+
+    __tablename__ = 'tags'
+
+    def __repr__(self):
+        """Show info about a user."""
+
+        u = self
+        return f"<User {u.id} {u.name}>"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+
+
+
+class PostTag(db.Model):
+    '''User Database.'''
+
+    __tablename__ = 'posttags'
+
+    def __repr__(self):
+        """Show info about a user."""
+
+        u = self
+        return f"<User {u.post_id} {u.tag_id}>"
+
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), primary_key=True, nullable=False)
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), primary_key=True, nullable=False)
